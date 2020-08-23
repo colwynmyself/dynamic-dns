142a3a06dd554b415f23a9fa0af53b89c89ca6e8
Colwyn Fritze-Moor <colwyn@colwyn.me>
2020-08-23 13:12:14 -0700

    Make the TTL 5m

--- src/update-record.sh
+++ src/update-record.sh
@@ -21,6 +21,6 @@ dns_record_id=$(echo "${dns_record_res}" | jq -r '.result | .[0] | .id')
 x=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}" \
   -H "Authorization: Bearer ${api_key}" \
   -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"${dns_record_name}\",\"content\":\"${my_ip}\",\"ttl\":120,\"proxied\":false}")
+  --data "{\"type\":\"A\",\"name\":\"${dns_record_name}\",\"content\":\"${my_ip}\",\"ttl\":300,\"proxied\":false}")
 
 echo "Editing zone ${zone} (${zone_id}). ${dns_record_name} (${dns_record_id}) -> ${my_ip}"
